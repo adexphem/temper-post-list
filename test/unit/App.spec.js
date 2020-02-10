4da7d8908b1487d665d8b553a12baa1107a4bb9f
adexphem <adexphem4real@gmail.com>
2020-02-10 03:30:12 +0100

    write some test

--- test/unit/App.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import App from "@/App";
-
-describe("App.vue", () => {
-  it("renders a vue instance", () => {
-    const wrapper = shallowMount(App);
-    expect(wrapper.isVueInstance()).toBe(true);
-  });
-});
